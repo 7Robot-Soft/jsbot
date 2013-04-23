9321367ad316378b30258822aa6dc707d54ed6e7
Élie Bouttier <elie.bouttier@free.fr>
2013-04-23 13:06:26 +0200

    Speed controller

--- jsbot.py
+++ jsbot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from jsread import jsread
-from settings import HOST, PORT, DEVICES, LIB, REGEXP
+from settings import *
 import argparse
 import sys
 sys.path.append("../atp")
@@ -10,6 +10,51 @@ import socket
 import pyinotify
 import re
 import time
+from threading import Thread
+
+class SpeedOrder(Thread):
+
+    # TODO : utiliser un mutex sur x et y, et utiliser une condition dans le run
+
+    def __init__(self, asserv, delay = DELAY):
+        super().__init__()
+        self.asserv = asserv
+        self.delay = delay
+        self.x = 0
+        self.old_x = 0
+        self.y = 0
+        self.old_y = 0
+        self.z = 0
+        self.old_z = 0
+
+    def update(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def run(self):
+        while True:
+            time.sleep(self.delay)
+            if self.x != self.old_x or self.y != self.old_y or self.z != self.old_z:
+                self.old_x = self.x
+                self.old_y = self.y
+                self.old_z = self.z
+                self.send_command(self.x, self.y, self.z)
+
+    def send_command(self, _x, _y, _z):
+        print(_x, _y, _z)
+        from math import floor, ceil
+        x = (_x * Xmax) / 32767
+        y = (_y * Ymax) / 32767
+        z = (_z * Zmax) / 65536 + Zmax / 2
+
+        left = - round((80 * (y + x)) / (80 + abs(x)))
+        right = - round((80 * (y - x)) / (80 + abs(x)))
+        z = round(z)
+
+        #print(left, right, angle)
+        print("[%+3d %+3d] (%4d)" %(right, left, z))
+        #self.asserv.speed(self.x, self.y)
 
 class Processor:
     def __init__(self, host, port):
@@ -19,26 +64,29 @@ class Processor:
         self.asserv = Channel(self.asserv_file.buffer,
                 lambda name, args: name, proto = 'asserv')
         self.states = None
+        self.speed = SpeedOrder(self.asserv)
+        self.speed.start()
 
     def event(self, axes, buttons):
         #print(axes, buttons)
+        self.speed.update(axes[0], axes[1], axes[2])
         if self.states:
             if self.states[0] == 0 and buttons[0] == 1:
-                print("feu !")
+                print("Button 1 pressed!")
             elif self.states[1] == 0 and buttons[1] == 1:
-                pass
+                print("Button 2 pressed!")
             elif self.states[2] == 0 and buttons[2] == 1:
-                pass
+                print("Button 3 pressed!")
             elif self.states[3] == 0 and buttons[3] == 1:
-                pass
+                print("Button 4 pressed!")
             elif self.states[4] == 0 and buttons[4] == 1:
-                pass
+                print("Button 5 pressed!")
             elif self.states[5] == 0 and buttons[5] == 1:
-                pass
+                print("Button 6 pressed!")
             elif self.states[6] == 0 and buttons[6] == 1:
-                pass
+                print("Button 7 pressed!")
             elif self.states[7] == 0 and buttons[7] == 1:
-                pass
+                print("Button 8 pressed!")
         self.states = buttons
 
 
--- settings.py
+++ settings.py
@@ -1,5 +1,20 @@
+
+# Host and base port for ATP communication
 HOST = "ash"
 PORT = 1300
+
+# Directory for joystick device research
 DEVICES = '/dev/input'
-LIB = 'libjsread.so'
+# Regexp to match devices
 REGEXP = '^js[0-9]+$'
+
+# jsread library
+LIB = 'libjsread.so'
+
+# Delay between two new message
+DELAY = 0.1
+
+
+Xmax = 80
+Ymax = 80
+Zmax = 1023
